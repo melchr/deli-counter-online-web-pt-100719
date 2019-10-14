8750cb2ea9462ac600d9f97289efb9da41dcbc58
Melissa Christman <melchrstmn@gmail.com>
2019-10-14 23:02:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+
+def line(katz_deli)
+  if katz_deli.empty?
+    puts "The line is currently empty."
+  else
+    puts katz_deli
+end
+
+def take_a_number(katz_deli, name)
+  puts katz_deli << name.length
+end
+
+def now_serving
+  puts katz_deli.shift
+end
